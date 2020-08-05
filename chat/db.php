bc8a345af78ccba07dd2b3305d94d5dcb9596f32
jepatouanne <jbtouanne@gmail.com>
2020-08-05 10:57:06 -0500

    Add files via upload

---
+++ chat/db.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "appchat2";
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
+function formatearfecha($fecha){
+  return date('g:i a', strtotime($fecha));
+}
+?>
